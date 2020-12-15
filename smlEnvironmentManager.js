5b983d8ec74b70fbcc45a6d70f003c8583ef2874
V. Julião <viniciusjuliao@dcc.ufmg.br>
2020-12-15 04:48:46 -0300

    start

---
+++ smlEnvironmentManager.js
@@ -0,0 +1,53 @@
+const spawn = require('child_process').spawn;
+const vscode = require('vscode');
+
+let sml;
+let smlOutput;
+
+function start(){
+	sml = spawn('sml');
+	
+	sml.stdin.setEncoding('utf-8');
+	sml.stdout.setEncoding('utf-8');
+	sml.stderr.setEncoding('utf-8');
+	console.log('started');
+	sml.stdin.read(0);
+	
+	sml.stderr.on('data', (data) => {
+		smlOutput.show(false);
+		smlOutput.append(data + `\n`);
+	});
+
+	sml.stdout.on('data', (data) => {
+		smlOutput.show(false);
+		smlOutput.append(data + `\n`);
+	});
+	smlOutput = vscode.window.createOutputChannel("SML");
+	smlOutput.show(false);
+}
+
+function execShortCode(){
+	if(!sml){
+		start();
+		setTimeout(()=>{execShortCode()}, 100);
+	} 
+	const editor = vscode.window.activeTextEditor;
+
+	if (editor) {
+		const document = editor.document;
+		const selection = editor.selection;
+
+		const code = document.getText(selection);
+		sml.stdin.write(code+';');
+	}
+}
+
+function stop(){
+	sml.stdin.end();
+}
+
+module.exports = {
+	start,
+	stop,
+	execShortCode
+}
