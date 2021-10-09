11b4d9fae88f4fa8a61f19bc5db8e2b9f95376b8
V. Julião <viniciusjuliao@dcc.ufmg.br>
2021-10-09 17:47:37 -0300

    indentation rolling back

--- smlEnvironmentManager.js
+++ smlEnvironmentManager.js
@@ -6,88 +6,86 @@ const smlOutput = vscode.window.createOutputChannel("SML");
 let allowNextCommand;
 
 function start() {
-  allowNextCommand = false;
-  const interpreter = vscode.workspace
-    .getConfiguration()
-    .get("sml-environment-interpreter-path", "sml");
+	allowNextCommand = false;
+	const interpreter = vscode.workspace
+	.getConfiguration()
+	.get("sml-environment-interpreter-path", "sml");
 
-  var cwd = {};
-  if (vscode.workspace.workspaceFolders !== undefined) {
-    var wd = vscode.workspace.workspaceFolders[0].uri.fsPath;
+	var cwd = {};
+	if (vscode.workspace.workspaceFolders !== undefined) {
+		var wd = vscode.workspace.workspaceFolders[0].uri.fsPath;
+		console.log("setting path to: " + wd);
+		cwd = { cwd: wd };
+	} else {
+		console.log("Unable to set working directory, no current workspace folder");
+	}
+	
+	sml = spawn(interpreter, [], Object.assign({ shell: true }, cwd));
+	
+	sml.stdin.setEncoding("utf-8");
+	sml.stdout.setEncoding("utf-8");
+	sml.stderr.setEncoding("utf-8");
+	console.log("started");
+	sml.stdin.read(0);
 
-    console.log("setting path to: " + wd);
+	sml.on("error", function (err) {
+		console.log(err);
+		smlOutput.append(err.message);
+	});
 
-    cwd = { cwd: wd };
-  } else {
-    console.log("Unable to set working directory, no current workspace folder");
-  }
+	sml.stderr.on("data", (data) => {
+		smlOutput.show(false);
+		smlOutput.append(data + `\n`);
+		allowNextCommand = true;
+	});
 
-  sml = spawn(interpreter, [], Object.assign({ shell: true }, cwd));
-
-  sml.stdin.setEncoding("utf-8");
-  sml.stdout.setEncoding("utf-8");
-  sml.stderr.setEncoding("utf-8");
-  console.log("started");
-  sml.stdin.read(0);
-
-  sml.on("error", function (err) {
-    console.log(err);
-    smlOutput.append(err.message);
-  });
-
-  sml.stderr.on("data", (data) => {
-    smlOutput.show(false);
-    smlOutput.append(data + `\n`);
-    allowNextCommand = true;
-  });
-
-  sml.stdout.on("data", (data) => {
-    smlOutput.show(false);
-    smlOutput.append(data + `\n`);
-  });
-  smlOutput.show(false);
+	sml.stdout.on("data", (data) => {
+		smlOutput.show(false);
+		smlOutput.append(data + `\n`);
+	});
+	smlOutput.show(false);
 }
 
 async function execShortCode() {
-  while (!sml && !allowNextCommand) {}
-  const editor = vscode.window.activeTextEditor;
+	while (!sml && !allowNextCommand) { ; }
+	const editor = vscode.window.activeTextEditor;
 
-  if (editor) {
-    const document = editor.document;
-    const selection = editor.selection;
+	if (editor) {
+		const document = editor.document;
+		const selection = editor.selection;
 
-    const code = document.getText(selection);
-    if (sml.exitCode === 0 || sml.exitCode)
-      vscode.window.showErrorMessage("SML process died");
-    else {
-      try {
-        allowNextCommand = false;
-        sml.stdin.write(code + ";;;;\r\n");
-      } catch (error) {
-        smlOutput.append(error.message);
-      }
-    }
-    await vscode.commands.executeCommand(
-      "workbench.action.terminal.scrollToBottom"
-    );
-  }
+		const code = document.getText(selection);
+		if (sml.exitCode === 0 || sml.exitCode)
+			vscode.window.showErrorMessage("SML process died");
+		else {
+			try {
+				allowNextCommand = false;
+				sml.stdin.write(code + ";;;;\r\n");
+			} catch (error) {
+				smlOutput.append(error.message);
+			}
+		}
+		await vscode.commands.executeCommand(
+			"workbench.action.terminal.scrollToBottom"
+		);
+	}
 }
-
+	
 function restart() {
-  if (sml.exitCode !== 0 && !sml.exitCode) {
-    sml.stdin.end();
-  }
-  sml.kill();
-  start();
+	if (sml.exitCode !== 0 && !sml.exitCode) {
+		sml.stdin.end();
+	}
+	sml.kill();
+	start();
 }
-
+	
 function stop() {
-  sml.stdin.end();
+	sml.stdin.end();
 }
 
 module.exports = {
-  start,
-  stop,
-  restart,
-  execShortCode,
+	start,
+	stop,
+	restart,
+	execShortCode,
 };
